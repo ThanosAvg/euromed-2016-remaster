09e49ebb71a59eb9e498b4cf66a2b2210ec3a47a
George Anastasiou <dr.george995@gmail.com>
2016-12-29 03:29:37 +0200

    Added bootstrap and jquery

--- src/views/footer.php
+++ src/views/footer.php
@@ -1,2 +1,7 @@
+<!-- JQUERY -->
+<script src="<?=PUBLIC_ROOT?>/assets/js/jquery-3.1.1.min.js"></script>
+
+<!-- BOOTSTRAP JS -->
+<script src="<?=PUBLIC_ROOT?>/assets/js/bootstrap.min.js"></script>
 </body>
 </html>
--- src/views/header.php
+++ src/views/header.php
@@ -2,5 +2,9 @@
 <head>
 <link rel="icon" href="<?=PUBLIC_ROOT?>/assets/img/favicon.ico" type="image/x-icon" />
 <title>Index Page</title>
+
+<!-- BOOTSTRAP CSS -->
+<link href="<?=PUBLIC_ROOT?>/assets/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
 <body>
