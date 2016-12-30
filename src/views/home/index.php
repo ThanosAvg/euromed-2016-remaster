ea8f290026701c2ed81bd44928b941e3c8c816f4
Narima <narimakikak@gmail.com>
2016-12-31 01:27:08 +0200

    basic front page

--- src/views/home/index.php
+++ src/views/home/index.php
@@ -1,2 +1,17 @@
-<h1>Hello World</h1>
-<a href="<?=PUBLIC_ROOT?>/test">Link to test</a>
+
+<div class="row">
+  <div class="col-md-3 side-panel">
+  	<h1><b>Dates</b></h1><br>
+  	<span class="side-text"> Date 31/10/2016</span><br>
+  	<span class="side-text">Date 5/11/2016</span><br><br>
+  	<h1><b>News</b></h1>
+  	<a class="side-text"-href="#">Link to test</a><br>
+  	<a class="side-text"href="#">Link to test</a>
+  </div>
+  <div class="col-md-9">
+  	<h1><b>Welcome</b></h1><br>
+  	<span class="welcome-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at sollicitudin eros. Suspendisse urna neque, vestibulum at erat quis, tempor lobortis nibh. Nunc ultrices turpis quis sapien cursus aliquam. Quisque porta vel lorem vehicula varius. Cras at velit at tortor fringilla ultrices pharetra vel orci. Quisque dignissim ornare nulla, ut varius enim suscipit vel. Ut pretium ex sapien, eu sollicitudin massa malesuada et. Vestibulum facilisis, ligula ut tristique varius, diam tortor ultricies ipsum, sed vestibulum arcu libero ut odio. Suspendisse auctor justo et dolor faucibus aliquet in vitae risus. Mauris facilisis neque massa, nec dictum leo semper in.
+  	Phasellus ullamcorper libero eu lacus hendrerit molestie. Nulla pellentesque magna non purus aliquet aliquet. Vivamus ultricies nisl quis justo feugiat, vel venenatis ligula cursus. Praesent in luctus turpis. Nam eleifend augue bibendum venenatis condimentum. Praesent quis ante ultrices, interdum libero non, sagittis mauris. Maecenas malesuada diam quam. Aliquam libero felis, condimentum vel justo dictum, mollis placerat turpis. Cras lacinia laoreet nunc. Etiam sit amet enim rutrum, luctus nulla nec, iaculis justo. Etiam malesuada viverra fermentum. Nunc nec libero at risus sagittis tincidunt. Nam nec tempus lectus.</span>
+  </div>
+</div>
+
