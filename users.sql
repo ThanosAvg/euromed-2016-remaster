b534a50f78c63956f17d650b6cba03ec87a73848
George Anastasiou <dr.george995@gmail.com>
2017-01-15 22:49:05 +0200

    Full database backup

--- users.sql
+++ users.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Φιλοξενητής: localhost
--- Χρόνος δημιουργίας: 15 Ιαν 2017 στις 05:04:14
+-- Χρόνος δημιουργίας: 15 Ιαν 2017 στις 21:48:01
 -- Έκδοση διακομιστή: 10.1.19-MariaDB
 -- Έκδοση PHP: 7.0.9
 
@@ -19,6 +19,19 @@ SET time_zone = "+00:00";
 --
 -- Βάση δεδομένων: `euromed`
 --
+CREATE DATABASE IF NOT EXISTS `euromed` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `euromed`;
+
+-- --------------------------------------------------------
+
+--
+-- Δομή πίνακα για τον πίνακα `user_has_file`
+--
+
+CREATE TABLE `user_has_file` (
+  `user_email` varchar(100) NOT NULL,
+  `file` mediumblob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -41,20 +54,24 @@ CREATE TABLE `users` (
   `newsletter` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
 --
--- Δομή πίνακα για τον πίνακα `user_has_file`
+-- Άδειασμα δεδομένων του πίνακα `users`
 --
 
-CREATE TABLE `user_has_file` (
-  `user_email` varchar(100) NOT NULL,
-  `file` mediumblob NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `birthday`, `password`, `country`, `city`, `address`, `zip`, `phone`, `newsletter`) VALUES
+(43, 'Richard', 'Stallman', 'stallman@stallman.com', NULL, '$2y$10$YJqxAtMkBnMZ07Go82hjWOxFoOHGVtIDQgSE4X94SOllhsyG2wbdq', NULL, NULL, NULL, NULL, NULL, 0),
+(44, 'Olga', 'Pavlova', 'olga@moscow.edu', NULL, '$2y$10$WtR7Qt/hRGmgzQ4bri3LxeAvpHbWu5wTXJZf18tbjxRB869FEaPRS', NULL, NULL, NULL, NULL, NULL, 0),
+(45, 'Alexis', 'Alexiou', 'alexis@corporate.com', NULL, '$2y$10$vp/QgSNTL.yyqdATsZEAq.hRH1uMKGJBlo7aKa3ZFQEnP931jDr36', NULL, NULL, NULL, NULL, NULL, 0);
 
 --
 -- Ευρετήρια για άχρηστους πίνακες
 --
+
+--
+-- Ευρετήρια για πίνακα `user_has_file`
+--
+ALTER TABLE `user_has_file`
+  ADD PRIMARY KEY (`user_email`);
 
 --
 -- Ευρετήρια για πίνακα `users`
@@ -64,12 +81,6 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `email` (`email`);
 
 --
--- Ευρετήρια για πίνακα `user_has_file`
---
-ALTER TABLE `user_has_file`
-  ADD PRIMARY KEY (`user_email`);
-
---
 -- AUTO_INCREMENT για άχρηστους πίνακες
 --
 
@@ -77,7 +88,7 @@ ALTER TABLE `user_has_file`
 -- AUTO_INCREMENT για πίνακα `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
